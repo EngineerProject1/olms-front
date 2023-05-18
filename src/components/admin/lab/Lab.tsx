6d67784ab340fbce4017b567a654979f25ea396d
lsh <3040114965@qq.com>
2023-05-18 22:33:16 +0800

    feat: lab deletion and batch deletion

--- src/components/admin/lab/Lab.tsx
+++ src/components/admin/lab/Lab.tsx
@@ -2,73 +2,76 @@ import { Image, Pagination, Popconfirm, Space } from 'antd'
 import Search from 'antd/es/input/Search'
 import Table from 'antd/es/table'
 import Button from 'antd/lib/button'
+import { GlobalContext } from 'app'
 import { LabDateType, LabModel, LabPageParams } from 'mdoel/LabModel'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import axios from 'tools/axios'
-const columns = [
-  {
-    title: '实验室名称',
-    dataIndex: 'name',
-    width: '12.5%',
-  },
-  {
-    title: '图片',
-    dataIndex: 'images',
-    width: '12.5%',
-    render: (images: any) => (
-      <Image width={60} src={`/api/img/download?name=${images}`}></Image>
-    ),
-  },
-  {
-    title: '负责人',
-    dataIndex: 'master',
-    width: '12.5%',
-  },
-  {
-    title: '地址',
-    dataIndex: 'location',
-    width: '12.5%',
-  },
-  {
-    title: '描述',
-    dataIndex: 'description',
-    width: '12.5%',
-  },
-  {
-    title: '容量',
-    dataIndex: 'capacity',
-    width: '12.5%',
-    sorter: true,
-  },
-  {
-    title: '类型',
-    dataIndex: 'type',
-    width: '12.5%',
-  },
-  {
-    title: '操作',
-    dataIndex: 'operation',
-    width: '12.5%',
-    render: (_: any, record: { key: React.Key }) => (
-      <Space>
-        <a
-          onClick={() => {
-            console.log(1)
-          }}>
-          编辑
-        </a>
-        <Popconfirm
-          title="是否删除该公告信息？"
-          onConfirm={() => {
-            console.log(2)
-          }}>
-          <a style={{ color: 'red' }}>删除</a>
-        </Popconfirm>
-      </Space>
-    ),
-  },
-]
+
 function Lab() {
+  const columns = [
+    {
+      title: '实验室名称',
+      dataIndex: 'name',
+      width: '12.5%',
+    },
+    {
+      title: '图片',
+      dataIndex: 'images',
+      width: '12.5%',
+      render: (images: any) => (
+        <Image width={60} src={`/api/img/download?name=${images}`}></Image>
+      ),
+    },
+    {
+      title: '负责人',
+      dataIndex: 'master',
+      width: '12.5%',
+    },
+    {
+      title: '地址',
+      dataIndex: 'location',
+      width: '12.5%',
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      width: '12.5%',
+    },
+    {
+      title: '容量',
+      dataIndex: 'capacity',
+      width: '12.5%',
+      sorter: true,
+    },
+    {
+      title: '类型',
+      dataIndex: 'type',
+      width: '12.5%',
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      width: '12.5%',
+      render: (_: any, record: { key: React.Key }) => (
+        <Space>
+          <a
+            onClick={() => {
+              console.log(1)
+            }}>
+            编辑
+          </a>
+          <Popconfirm
+            title="是否删除该公告信息？"
+            onConfirm={() => {
+              delLab(record.key)
+            }}>
+            <a style={{ color: 'red' }}>删除</a>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ]
+  const { messageApi } = useContext(GlobalContext)
   // 查询的条件
   const [params, setParams] = useState<LabPageParams>({
     page: 1,
@@ -77,21 +80,6 @@ function Lab() {
     name: '',
     capacity: '',
   })
-
-  // 设置排序条件
-  const onChange = ({
-    pagination,
-    filter,
-    sorter,
-  }: {
-    pagination: any
-    filter: any
-    sorter: any
-  }) => {
-    const field = sorter.field
-    const order = sorter.order ? sorter.order.replace('end', '') : ''
-    setParams({ ...params, capacity: order })
-  }
 
   // 实验室数据
   const [data, setData] = useState<LabDateType[]>()
@@ -139,10 +127,60 @@ function Lab() {
     })
   }, [params.capacity, params.name, params.page, params.pageSize, params.total])
 
+  // 设置排序条件
+  const onChange = ({
+    pagination,
+    filter,
+    sorter,
+  }: {
+    pagination: any
+    filter: any
+    sorter: any
+  }) => {
+    const field = sorter.field
+    const order = sorter.order ? sorter.order.replace('end', '') : ''
+    setParams({ ...params, capacity: order })
+  }
+  // 单个删除
+  const delLab = (id: React.Key) => {
+    axios.delete(`/auth/lab/${id}`).then((resp) => {
+      const data = resp.data
+      if (data.code === 200) {
+        messageApi.success(data.msg)
+      } else {
+        messageApi.error(data.error)
+      }
+      setParams({ ...params, total: params.total - 1 })
+    })
+  }
+
+  // 批量删除
+  const delBatch = () => {
+    axios
+      .delete('/auth/lab', {
+        data: {
+          ids: selectedRowKeys,
+        },
+      })
+      .then((resp) => {
+        const data = resp.data
+        if (data.code === 200) {
+          messageApi.success(data.msg)
+        } else {
+          messageApi.error(data.error)
+        }
+        setParams({
+          ...params,
+          total: params.total - selectedRowKeys.length,
+        })
+        setSelectedRowKeys([])
+      })
+  }
+
   return (
     <>
       {/* 搜索框 */}
-      <div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Search
           placeholder="实验室名字"
           onSearch={(e) => {
@@ -151,13 +189,33 @@ function Lab() {
           style={{ width: 200 }}
           size="large"
         />
-        {/* 新增实验室 */}
-        <Button
-          onClick={() => console.log('新增设备')}
-          type="primary"
-          style={{ marginBottom: 16, marginRight: 105, float: 'right' }}>
-          + 新增实验室
-        </Button>
+        <div
+          style={{
+            width: 180,
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginRight: '100px',
+          }}>
+          {/* 批量删除 */}
+          <Popconfirm
+            title="是否删除所选公告信息？"
+            onConfirm={delBatch}
+            disabled={selectedRowKeys.length === 0}>
+            <Button
+              danger
+              disabled={selectedRowKeys.length === 0}
+              style={{ marginRight: '16px' }}>
+              删除选中
+            </Button>
+          </Popconfirm>
+          {/* 新增实验室 */}
+          <Button
+            onClick={() => console.log('新增设备')}
+            type="primary"
+            style={{ marginBottom: 16, marginRight: 105, float: 'right' }}>
+            + 新增实验室
+          </Button>
+        </div>
       </div>
       {/* 表格 */}
       <Table
